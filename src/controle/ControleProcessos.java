efe0db47080ee116e0c377c376e1b8e29dab5312
TatianePaz <@TatianePaz>
2017-07-24 00:21:36 -0300

    Merge ControleProcessos

--- src/controle/ControleProcessos.java
+++ src/controle/ControleProcessos.java
@@ -14,16 +14,20 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import modelo.Processo;
 
 /**
  *
  * @author IgorCamargo
+ * @author Tatiane Paz
  */
 public class ControleProcessos implements Initializable {
     
     //ArrayList de processos
     List<Processo> processos = new ArrayList<Processo>();
-                  
+    List <int> listaPrioridades = new List<int>();             
+    
+    
     @FXML
     private Label label;
     
@@ -59,74 +63,5 @@ public class ControleProcessos implements Initializable {
         
     }
 
-public static class Processo {
-    private Color cor;
-    private int pid;
-    private int prioridade;
-    private String estado;
-    private int tempoCPU;
-    private int qtdFrames;
-    private String tipoProcesso;
-    //Tempo de execucao
-    private static float quantum;
-    //{EXECUTANDO,PRONTO,BLOQUEADO,AGUARDANDO}
-    private static String status;
-
-    public Color getCor() {
-        return cor;
-    }
-
-    public void setCor(Color cor) {
-        this.cor = cor;
-    }
-
-    public int getPid() {
-        return pid;
-    }
-
-    public void setPid(int pid) {
-        this.pid = pid;
-    }
-
-    public int getPrioridade() {
-        return prioridade;
-    }
-
-    public void setPrioridade(int prioridade) {
-        this.prioridade = prioridade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getTempoCPU() {
-        return tempoCPU;
-    }
-
-    public void setTempoCPU(int tempoCPU) {
-        this.tempoCPU = tempoCPU;
-    }
-
-    public int getQtdFrames() {
-        return qtdFrames;
-    }
-
-    public void setQtdFrames(int qtdFrames) {
-        this.qtdFrames = qtdFrames;
-    }
-
-    public String getTipoProcesso() {
-        return tipoProcesso;
-    }
-
-    public void setTipoProcesso(String tipoProcesso) {
-        this.tipoProcesso = tipoProcesso;
-    }
-
-}
+    
 }
